c80944a39e139621c4da46e4a90c71466c592d07
Romina Gonzalez <rominagonzalezn29@gmail.com>
2022-12-07 23:26:46 -0300

    Componente producto

--- src/Componentes/Productos/index.js
+++ src/Componentes/Productos/index.js
@@ -1,9 +1,27 @@
 import React from "react";
-
+import img from '../assets/images/img01.jpg'
 export const Productos = () => {
     return (
-        <div>
-            <h1> Productos</h1>
+        <>
+        <h1 className="title"> Productos</h1>
+        <div className="productos">
+            <a href="#">
+            <div className="producto">
+            <img src={img} alt =''/>
+            </div>
+            </a>
+            <div className="producto_footer">
+                <h1>Title</h1>
+                <p>Categoria</p>
+                <p className="price">$320</p>
+            </div>
+            <div className="button">
+                <button className="btn"> A;adir al carrito </button>
+                <div>
+                    <a href="#" className="btn"> Vista </a>
+                </div>
+            </div>
         </div>
+        </>
     )
 }
